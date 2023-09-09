5bc78e6ef8a27f0cede43c12beac676782590b04
Janik Viertel <janik@next-data-service.com>
2023-09-09 18:59:21 +0200

    added git clone functionality

--- jobs/bi_weekly.py
+++ jobs/bi_weekly.py
@@ -1,17 +1,8 @@
-import os
-
 from apscheduler.triggers.cron import CronTrigger
-from loguru import logger
-
+from quarter_lib.logging import setup_logging
 from services.request import trigger_job
 
-logger.add(
-
-    os.path.join(os.path.dirname(os.path.abspath(__file__)) + "/logs/" + os.path.basename(__file__) + ".log"),
-    format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-    backtrace=True,
-    diagnose=True,
-)
+logger = setup_logging(__name__)
 
 ROUTER_NAME = "bi_weekly"
 
@@ -19,16 +10,26 @@ ROUTER_NAME = "bi_weekly"
 def add_jobs(scheduler):
     scheduler.add_job(
         lambda: trigger_job(ROUTER_NAME, "update_to_think_about"),
-        CronTrigger.from_crontab("54 23 1/3 * *"),
+        CronTrigger.from_crontab("54 23 2/10 * *"),
         id="update_to_think_about",
     )
     scheduler.add_job(
-        lambda: trigger_job(ROUTER_NAME, "obsidian"),
-        CronTrigger.from_crontab("54 23 2/3 * *"),
-        id="obsidian",
+        lambda: trigger_job(ROUTER_NAME, "obsidian_random_note"),
+        CronTrigger.from_crontab("54 23 4/10 * *"),
+        id="obsidian_random_note",
+    )
+    scheduler.add_job(
+        lambda: trigger_job(ROUTER_NAME, "obsidian_random_activity"),
+        CronTrigger.from_crontab("54 23 6/10 * *"),
+        id="obsidian_random_activity",
+    )
+    scheduler.add_job(
+        lambda: trigger_job(ROUTER_NAME, "obsidian_oldest_note"),
+        CronTrigger.from_crontab("54 23 8/10 * *"),
+        id="obsidian_oldest_note",
     )
     scheduler.add_job(
         lambda: trigger_job(ROUTER_NAME, "update_book_rework"),
-        CronTrigger.from_crontab("57 23 3/3 * *"),
+        CronTrigger.from_crontab("57 23 10/10 * *"),
         id="update_book_rework"
     )

96bbde0d85121fc4d3dcb526940d54580ce51d3c
viertel97 <janik.viertel@outlook.com>
2023-10-23 18:53:51 +0200

    updated routines

--- jobs/bi_weekly.py
+++ jobs/bi_weekly.py
@@ -9,27 +9,31 @@ ROUTER_NAME = "bi_weekly"
 
 def add_jobs(scheduler):
     scheduler.add_job(
+        lambda: trigger_job(ROUTER_NAME, "update_book_rework"),
+        CronTrigger.from_crontab("57 23 1,5,9,13,17,21,25,29 * *"),
+        id="update_book_rework"
+    )
+
+    scheduler.add_job(
         lambda: trigger_job(ROUTER_NAME, "update_to_think_about"),
-        CronTrigger.from_crontab("54 23 2/10 * *"),
+        CronTrigger.from_crontab("54 23 3,19 * *"),
         id="update_to_think_about",
     )
+
     scheduler.add_job(
         lambda: trigger_job(ROUTER_NAME, "obsidian_random_note"),
-        CronTrigger.from_crontab("54 23 4/10 * *"),
+        CronTrigger.from_crontab("54 23 7,23 * *"),
         id="obsidian_random_note",
     )
-    scheduler.add_job(
-        lambda: trigger_job(ROUTER_NAME, "obsidian_random_activity"),
-        CronTrigger.from_crontab("54 23 6/10 * *"),
-        id="obsidian_random_activity",
-    )
+
     scheduler.add_job(
         lambda: trigger_job(ROUTER_NAME, "obsidian_oldest_note"),
-        CronTrigger.from_crontab("54 23 8/10 * *"),
+        CronTrigger.from_crontab("54 23 11,27 * *"),
         id="obsidian_oldest_note",
     )
+
     scheduler.add_job(
-        lambda: trigger_job(ROUTER_NAME, "update_book_rework"),
-        CronTrigger.from_crontab("57 23 10/10 * *"),
-        id="update_book_rework"
+        lambda: trigger_job(ROUTER_NAME, "obsidian_random_activity"),
+        CronTrigger.from_crontab("54 23 15,31 * *"),
+        id="obsidian_random_activity",
     )

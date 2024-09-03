bd249a142242e363eb48014b011bda47200354af
Janik Viertel <janik@next-data-service.com>
2024-09-03 13:28:14 +0300

    added retrying

--- services/request.py
+++ services/request.py
@@ -2,7 +2,7 @@ import platform
 import os
 import requests as requests
 from quarter_lib.logging import setup_logging
-
+import time
 from services.telegram_service import send_to_telegram
 
 logger = setup_logging(__file__)
@@ -17,12 +17,28 @@ else:
 
 logger.info("API_IP: " + API_IP)
 
+RETRIES = 5
+WAIT_TIME = 2
+
 def trigger_job(router, job_id):
     url = API_IP + router + "/" + job_id
-    logger.info("trigger job: " + url)
-    result = requests.post(url)
-    if result.status_code != 200:
-        logger.error("trigger job failed: " + str(result))
-        send_to_telegram("trigger job (" + job_id + ") failed: " + str(result))
-    else:
-        logger.info("trigger job result: " + str(result))
+
+    for attempt in range(RETRIES):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Check if the request was successful
+            # try to get json from response, otherwise log result object
+            try:
+                result_json = response.json()
+            except Exception as e:
+                result_json = response
+            logger.info(f"Response for URL {url}: {result_json}")
+            return response.json()  # Return the response if successful
+        except requests.exceptions.RequestException as e:
+            if attempt < RETRIES - 1:  # Only wait if there are retries left
+                logger.error(f"Attempt for URL {url} failed: {e}. Retrying in {WAIT_TIME} seconds.")
+                send_to_telegram(f"Attempt for URL {url} failed: {e}. Retrying in {WAIT_TIME} seconds.")
+                time.sleep(WAIT_TIME)
+            else:
+                send_to_telegram(f"Attempt for URL {url} failed: {e}. No more retries left.")
+                raise  # Re-raise the exception if all retries fail
